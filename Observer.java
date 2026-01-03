f20bef45c12408d8b682d740bb4dd9900d4f3aaa
Xsawier <ksawier.topczewski@gmail.com>
2026-01-03 21:41:13 +0100

    Added Observers (not working still)
    wrzuciłem "postęp" żeby nie było że nie było. Jutro po robocie naprawię miałem dzisiaj ale z okazji urodzin po robocie byłem cały dzień poza domem.

--- Observer.java
+++ Observer.java
@@ -1,3 +1,44 @@
 public interface Observer {
     void update();
 }
+
+class CaloriesObserver implements Observer{
+    private final MealComponent subject;
+    private int currentCalories;
+
+    public CaloriesObserver(MealComponent subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public void update() {
+        currentCalories = subject.getTotalCalories();
+    }
+
+    public int getCurrentCalories()
+    {
+        return currentCalories;
+    }
+}
+/* nie działa jeszcze 
+class ShoppingListObserver implements Observer{
+    private final DayPlan dayPlan;
+    private final Set<String> shoppingList = new HashSet<>();
+
+    public ShoppingListObserver(DayPlan dayPlan) {
+        this.dayPlan = dayPlan;
+    }
+
+    @Override
+    public void update() {
+        shoppingList.clear();
+
+        for(Meal meal: dayPlan.getMeals().values()) {
+            //shoppingList.addAll(meal.getRecipe().c
+        }
+    }
+
+    public Set<String> getShoppingList() {
+        return Collections.unmodifiableSet(shoppingList);
+    }
+}*/
